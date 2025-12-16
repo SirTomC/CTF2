e1cbd4e0039d253b5c96b558617300158ea8c7f6
Tommy Chen <tomc@udel.edu>
2025-12-15 22:30:58 -0500

    Final changes to CTF2 please

--- app.py
+++ app.py
@@ -5,37 +5,48 @@ app = Flask(__name__)
 
 @app.route('/')
 def calc():
-    op = request.args.get('op')
-    a = float(request.args.get('x'))
-    b = float(request.args.get('y'))
+    try:
+        # Get values with fallback defaults
+        op = request.args.get('op')
+        x = request.args.get('x')
+        y = request.args.get('y')
 
-    if op == 'add':
-        result = a + b
-    elif op == 'sub':
-        result = a - b
-    elif op == 'mul':
-        result = a * b
-    elif op == 'div':
-        result = a / b 
-    elif op == 'pow':
-        result = a ** b
-    else:
-        raise ValueError("Unsupported operation")
-    
-    return f"""
-        <h3>Calculator</h3>
-        <p><b>Result:</b> {result}</p>
-    """
+        # If no parameters provided, just show page with no result
+        if not op or not x or not y:
+            return '''
+                <h3>Calculator</h3>
+                <p>Provide parameters like <code>?op=add&x=1&y=2</code></p>
+            '''
 
-@app.errorhandler(500)
-def error(e):
-    return f"""
-        <h3>Calculator</h3>
-        <p>Oops, something went wrong!</p>
-        <pre>{e}</pre>
-        <hr>
-        <code>Tommy{{pLz_Us3_As_1nT3nd3d}}</code>
-    """, 500
+        a = float(x)
+        b = float(y)
+
+        if op == 'add':
+            result = a + b
+        elif op == 'sub':
+            result = a - b
+        elif op == 'mul':
+            result = a * b
+        elif op == 'div':
+            result = a / b
+        elif op == 'pow':
+            result = a ** b
+        else:
+            raise ValueError("Unsupported operation")
+
+        return f"""
+            <h3>Calculator</h3>
+            <p><b>Operation:</b> {op}({a}, {b})</p>
+            <p><b>Result:</b> {result}</p>
+        """
+
+    except Exception as e:
+        return f"""
+            <h3>Calculator</h3>
+            <p>Oops, something went wrong!</p>
+            <pre>{e}</pre>
+            <hr><code>Tommy{{pLz_Us3_As_1nT3nd3d}}</code>
+        """, 500
 
 if __name__ == '__main__':
     port = int(os.environ.get("PORT", 5000))
