054ea6d9a038b06439ccef9adad9a93b677ba666
Tommy Chen <tomc@udel.edu>
2025-12-15 22:12:34 -0500

    CTF2 Fixes

--- app.py
+++ app.py
@@ -17,4 +17,5 @@ def error(e):
     return f"<h3>Oops, something went wrong!</h3><pre>{e}</pre><hr><code>Tommy{{pLz_Us3_As_1nT3nd3d}}</code>", 500
 
 if __name__ == '__main__':
-    app.run(debug=True)
+    port = int(os.environ.get("PORT", 5000))  # required for Render
+    app.run(host='0.0.0.0', port=port)
